524c00c63a0e32979176a5141bbabd64131ad212
kkooff2000 <kkooff2000@gmail.com>
2016-06-14 20:21:30 +0800

    Add ActiveRecord Transformer prototype.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in activerecord_to_google_spreadsheet.gemspec
-gemspec
+gemspec :name => 'activerecord_to_google_spreadsheet'
 gem 'google_drive'
--- lib/activerecord_to_google_spreadsheet.rb
+++ lib/activerecord_to_google_spreadsheet.rb
@@ -1,7 +1,24 @@
 require "activerecord_to_google_spreadsheet/version"
 require "google_drive"
 module ActiveRecordToGoogleSpreadsheet
-  def self.get_db_info()
+  class << self
+    attr_accessor :configuration
+  end
+
+  class Configuration
+    attr_accessor :client_id, :client_secret, :redirect_uri
+
+    def initialize
+
+    end
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield self.configuration
+  end
+
+  def self.get_db_info
     info = {}
     i = 1
     ActiveRecord::Base.connection.tables.map do |model|
@@ -16,19 +33,9 @@ module ActiveRecordToGoogleSpreadsheet
     return info
   end
 
-  def self.get_worksheet_by_name(st, name)
-    ws = st.worksheet_by_title(name)
-    if !ws
-      ws = st.add_worksheet(name)
-    end
-    return ws
-  end
-
   def self.to_spreadsheet(session, spreadsheet_key)
-    info = get_db_info
-
     st = session.spreadsheet_by_key(spreadsheet_key)
-
+    info = get_db_info
     start_time = Time.now
     info.each do |name, value|
 
@@ -44,7 +51,6 @@ module ActiveRecordToGoogleSpreadsheet
           ws[index+2, i+1] = val[col]
         end
       end
-
       ws.save
       ws.reload
     end
@@ -78,32 +84,97 @@ module ActiveRecordToGoogleSpreadsheet
     puts '*************************'
   end
 
-  def self.get_session(code)
+  def self.each_tables
+  end
+
+  def self.setup_session(code)
     if $session
       return $session
     end
-    auth = get_auth()
+    auth = get_auth
     auth.code = code
     auth.fetch_access_token!
     $session = GoogleDrive.login_with_oauth(auth.access_token)
     return $session
   end
 
+  def self.get_session
+    if $session
+      return $session
+    else
+      raise GetSessionException
+    end
+  end
+
+  class GetSessionException < Exception
+    def message
+      "call setup_session(code) before get_session"
+    end
+  end
+
   def self.get_auth
     client = Google::APIClient.new
     auth = client.authorization
-    auth.client_id = "1079217375076-8nksnj4drtu77ni3rfrm9a7cv9pns7db.apps.googleusercontent.com"
-    auth.client_secret = "_j52ipkBFLzBdJT7bLljm9ub"
+    auth.client_id =  self.configuration.client_id
+    auth.client_secret = self.configuration.client_secret
     auth.scope =
       "https://www.googleapis.com/auth/drive " +
       "https://spreadsheets.google.com/feeds/"
-    auth.redirect_uri = "http://127.0.0.1:3000/google/write"
+    auth.redirect_uri = self.configuration.redirect_uri
     return auth
   end
 
   def self.google_login_url
-    auth = get_auth()
+    auth = get_auth
     auth_url = auth.authorization_uri
     return auth_url.to_s
+  end
+
+  module ActiveRecordBaseTransformer
+    def to_google_spreadsheet
+      puts '****************************base test************'
+      clazz = self.model_name.name.capitalize.singularize.camelize.to_s.constantize
+      puts clazz.column_names
+      clazz.column_names.each do |name|
+        puts self[name]
+      end
+    end
+
+    def from_google_spreadsheet
+    end
+  end
+
+  module ActiveRecordRelationTransformer
+    def to_google_spreadsheet(session, spreadsheet_key, name = self.table_name = self.table_name, worksheet_title = false)
+      puts '****************************relation test************'
+      puts self.column_names
+      puts self.table_name
+
+      self.each do |item|
+        self.column_names.each do |name|
+          puts item[name]
+        end
+      end
+    end
+
+    def from_google_spreadsheet
+    end
+  end
+
+  class ActiveRecord::Base
+    include ActiveRecordBaseTransformer
+  end
+
+  class ActiveRecord::Relation
+    include ActiveRecordRelationTransformer
+  end
+
+  private
+  def self.get_worksheet_by_name(st, name)
+    ws = st.worksheet_by_title(name)
+    if !ws
+      ws = st.add_worksheet(name)
+    end
+    return ws
   end
 end
